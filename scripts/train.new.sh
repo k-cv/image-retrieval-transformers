7c9aec8ac82e0fb0e959d514a344e24d0589e7a7
kfujii <kfujii@cv.info.gifu-u.ac.jp>
2024-12-09 09:35:12 +0900

    デバッグデータで

--- scripts/train.new.sh
+++ scripts/train.new.sh
@@ -1,17 +1,17 @@
 # traiｎ
 python new_main.py \
   --model deit_small_distilled_patch16_224 \
-  --max-iter 200 \
+  --max-iter 2000 \
   --dataset misumi \
-  --data-path /home/kfujii/vitruvion/data/images/tmp \
-  --geo-features-path /home/kfujii/vitruvion/outputs/2024-10-21/14-40-41/features_labels.pth \
   --rank 1 2 4 8 \
   --lambda-reg 0.7 \
-  --test-ratio 0.2
-  # --data-path /home/kfujii/drawing/data \
+  --test-ratio 0.2 \
+  --data-path /home/kfujii/vitruvion/data/images/tmp \
+  --geo-features-path /home/kfujii/vitruvion/outputs/2024-12-07/12-39-31/features_paths.pth \
+  --seed 0
+  # --data-path /home/kfujii/vitruvion/data/images/tmp \
   # --geo-features-path /home/kfujii/vitruvion/outputs/2024-10-21/14-40-41/features_labels.pth \
-
-
+  # --data-path /home/kfujii/drawing/data \
 
 # Zero-shot
 # python new_main.py \
